86acf671e5f00a2a520e1fe68cb0a47bc765bbe9
Reni Dantas <renidantas0@gmail.com>
2023-08-05 07:10:24 -0300

    Feat: adicionando CORS e adicionando subdomínio da netlify

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import jobs
 from fastapi import FastAPI, APIRouter
 from fastapi.responses import HTMLResponse, JSONResponse
 from api.routers import actions, commands, backups, logs
+from fastapi.middleware.cors import CORSMiddleware
 
 
 async def not_found(request, exc):
@@ -20,6 +21,17 @@ r.start()
 
 
 app = FastAPI(exception_handlers=exceptions)
+
+origins = ["http://127.0.0.1:5500", "https://64ce1fdcbe3d0c23fe0eb0c9--gleeful-selkie-8484e4.netlify.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(actions.router)
 app.include_router(commands.router)
 app.include_router(backups.router)
